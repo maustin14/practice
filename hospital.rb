9787c4fd84dcc0ce6f20cdd7454d74bb25ed7b87
Montrell Austin <montrell.austin@gmail.com>
2018-06-18 20:29:18 +0000

    Practice round 1

---
+++ hospital.rb
@@ -0,0 +1,5 @@
+dead_people = ["Ethel", "Michael", "Dan"]
+alive_people = ["George", "Frank", "Lucas"]
+
+dead_people.include? "Michael"
+dead_people.include? "George"
---
+++ loop.rb
@@ -0,0 +1,12 @@
+# i = 0
+
+# until i >= 11
+#     puts i
+#     i += 1
+# end
+# 
+# arr = [1.2.3]
+# counter = 0
+
+# while counter
+#
